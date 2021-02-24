4d59cc6a9b2f514354b6c02cf90fe10459b89771
Angelina.Leshkis <Angelina.Leshkis@gmail.com>
2021-02-24 17:11:31 +0300

    edit getting of character stream

--- src/main/java/com/example/demo/serviceimpl/TaskStream.java
+++ src/main/java/com/example/demo/serviceimpl/TaskStream.java
@@ -3,25 +3,24 @@ package com.example.demo.serviceimpl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.groupingBy;
 
 public class TaskStream {
 
-    public Map<Character, Integer> countOfTheMostCommonCharacter(String str) {
+    public Map<Character, Integer> countOfTheMostCommonCharacter(String input) {
 
         Map<Character, Integer> result = new HashMap<>();
 
-        Integer count = str.chars()
-                .mapToObj(symbol -> (char) symbol)
+        Integer count = getCharacterStream(input)
                 .collect(groupingBy(Character::charValue))
                 .values().stream()
                 .map(List::size)
                 .mapToInt(Integer::valueOf)
                 .max().orElse(0);
 
-        Character value = str.chars()
-                .mapToObj(symbol -> (char) symbol)
+        Character value = getCharacterStream(input)
                 .collect(groupingBy(Character::charValue))
                 .entrySet().stream()
                 .filter(characterListEntry -> count.equals(characterListEntry.getValue().size()))
@@ -31,6 +30,10 @@ public class TaskStream {
 
         return result;
 
+    }
+
+    private Stream<Character> getCharacterStream (String input) {
+        return input.chars().mapToObj(symbol -> (char) symbol);
     }
 
 }
